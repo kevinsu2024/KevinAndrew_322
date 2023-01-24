b542fbb8b8e24f79950791ff65b2f4e37fb9b370
Andrew W Lee <awl3193@moore.wot.eecs.northwestern.edu>
2023-01-23 21:11:19 -0600

    gen kill done

--- 322_framework/L2/src/liveness_generator.cpp
+++ 322_framework/L2/src/liveness_generator.cpp
@@ -20,6 +20,17 @@ namespace L2{
         std::set<std::string> out;
 
     }
+
+    std::string check_item(Item* it){
+        if(it->get_name() == "Register"){
+                Register* f = (Register*) it;
+                return f->get_register_ID();
+            } else if (it->get_name() == "Variable"){
+                Variable* f = (Variable*) it;
+                return f->get_variable_name();
+            } 
+            return "";
+    }
     int64_t find_label(std::string target_label, std::vector<Instruction*>* instructions){
         for (int i = 0; i < instructions->size(); i++){
             if ((*instructions)[i]->get_name() == "Instruction_label"){
@@ -31,6 +42,159 @@ namespace L2{
             }
         }
         return -1;
+    }
+
+    void gen_kill(Node* n){
+        Instruction* in = n->instr;
+        std::cout << "\nxdxd\n";
+        std::cout << in->get_name();
+        if(in->get_name() == "Instruction_ret"){
+            return;
+        } else if (in->get_name() == "Instruction_cjump"){
+            Instruction_cjump* i = (Instruction_cjump*) in;
+            Item* first = i->get_first();
+            Item* second = i->get_second();
+            if (check_item(first) != ""){
+                n->gen.insert(check_item(first));
+            }
+            if (check_item(second) != ""){
+                n->gen.insert(check_item(second));
+            }
+        } else if (in->get_name() == "Instruction_call_u"){
+            Instruction_call_u* i = (Instruction_call_u*) in;
+            Item* item = i->get_u();
+            if (check_item(item) != ""){
+                n->gen.insert(check_item(item));
+            }
+        } else if (in->get_name() == "Instruction_pp"){
+            Instruction_pp* i = (Instruction_pp*) in;
+            Item* item = i->get_reg();
+            if (check_item(item) != ""){
+                n->gen.insert(check_item(item));
+                n->kill.insert(check_item(item));
+            }
+        } else if (in->get_name() == "Instruction_mm"){
+            Instruction_mm* i = (Instruction_mm*) in;
+            Item* item = i->get_reg();
+            if (check_item(item) != ""){
+                n->gen.insert(check_item(item));
+                n->kill.insert(check_item(item));
+            }
+        } else if (in->get_name() == "Instruction_at"){
+            Instruction_at* i = (Instruction_at*) in;
+            Item* item = i->get_reg1();
+            Item* item2 = i->get_reg2();
+            Item* item3 = i->get_reg3();
+            if (check_item(item) != ""){
+                n->kill.insert(check_item(item));
+            }
+            if (check_item(item2) != ""){
+                n->gen.insert(check_item(item2));
+            }
+            if (check_item(item3) != ""){
+                n->gen.insert(check_item(item3));
+            }
+        } else if (in->get_name() == "Instruction_cmp_assignment"){
+            Instruction_cmp_assignment* i = (Instruction_cmp_assignment*) in;
+            Item* item = i->get_dst();
+            Item* item2 = i->get_first();
+            Item* item3 = i->get_second();
+            if (check_item(item) != ""){
+                n->kill.insert(check_item(item));
+            }
+            if (check_item(item2) != ""){
+                n->gen.insert(check_item(item2));
+            }
+            if (check_item(item3) != ""){
+                n->gen.insert(check_item(item3));
+            }
+        }  else if (in->get_name() == "Instruction_function_assignment"){
+            Instruction_function_assignment* i = (Instruction_function_assignment*) in;
+            Item* item = i->get_dst();
+            if (check_item(item) != ""){
+                n->kill.insert(check_item(item));
+            }
+        } else if (in->get_name() == "Instruction_stackarg_assignment"){
+            Instruction_stackarg_assignment* i = (Instruction_stackarg_assignment*) in;
+            Item* item = i->get_dst();
+            if (check_item(item) != ""){
+                n->kill.insert(check_item(item));
+            }
+        } else if (in->get_name() == "Instruction_assignment"){
+            Instruction_assignment* i = (Instruction_assignment*) in;
+            std::cout << "herererer\n\n";
+            
+
+            Item* first = i->get_dst();
+            std::cout << check_item(first) << "\n";
+            Item* second = i->get_src();
+            if (check_item(first) != ""){
+                n->kill.insert(check_item(first));
+            }
+            if (check_item(second) != ""){
+                n->gen.insert(check_item(second));
+            }
+        } else if (in->get_name() == "Instruction_mem_op_load"){
+            Instruction_mem_op_load* i = (Instruction_mem_op_load*) in;
+            Item* first = i->get_dst();
+            Item* second = i->get_src();
+            if (check_item(first) != ""){
+                n->kill.insert(check_item(first));
+            }
+            if (check_item(second) != "" && check_item(second) != "rsp"){
+                n->gen.insert(check_item(second));
+            }
+        } else if (in->get_name() == "Instruction_mem_load"){
+            Instruction_mem_load* i = (Instruction_mem_load*) in;
+            Item* first = i->get_dst();
+            Item* second = i->get_src();
+            if (check_item(first) != ""){
+                n->kill.insert(check_item(first));
+            }
+            if (check_item(second) != "" && check_item(second) != "rsp"){
+                n->gen.insert(check_item(second));
+            }
+        } else if (in->get_name() == "Instruction_mem_op_store"){
+            Instruction_mem_op_store* i = (Instruction_mem_op_store*) in;
+            Item* first = i->get_src();
+            Item* second = i->get_x_reg();
+            if (check_item(first) != ""){
+                n->gen.insert(check_item(first));
+            }
+            if (check_item(second) != "" && check_item(second) != "rsp"){
+                n->gen.insert(check_item(second));
+            }
+        } else if (in->get_name() == "Instruction_mem_store"){
+            Instruction_mem_store* i = (Instruction_mem_store*) in;
+            Item* first = i->get_src();
+            Item* second = i->get_x_reg();
+            if (check_item(first) != ""){
+                n->gen.insert(check_item(first));
+            }
+            if (check_item(second) != "" && check_item(second) != "rsp"){
+                n->gen.insert(check_item(second));
+            }
+        } else if (in->get_name() == "Instruction_aop"){
+            Instruction_aop* i = (Instruction_aop*) in;
+            Item* item = i->get_dst();
+            Item* item2 = i->get_src();
+            if (check_item(item) != ""){
+                n->kill.insert(check_item(item));
+            }
+            if (check_item(item2) != ""){
+                n->gen.insert(check_item(item2));
+            }
+        } else if (in->get_name() == "Instruction_sop"){
+            Instruction_sop* i = (Instruction_sop*) in;
+            Item* item = i->get_dst();
+            Item* item2 = i->get_src();
+            if (check_item(item) != ""){
+                n->kill.insert(check_item(item));
+            }
+            if (check_item(item2) != ""){
+                n->gen.insert(check_item(item2));
+            }
+        } 
     }
     
 
@@ -51,8 +215,8 @@ namespace L2{
                 int64_t succ_position = find_label(label_name, &instructs);
                 nodes[i]->succs.insert(succ_position);
                 nodes[succ_position]->preds.insert(i);
-                std::cout << "succ position: " << succ_position << "\n";
-                std::cout << "pred position: " << i << "\n";
+                // std::cout << "succ position: " << succ_position << "\n";
+                // std::cout << "pred position: " << i << "\n";
             } else if (in->get_name() == "Instruction_ret" || in->get_name() == "Instruction_call_tensor_error"){
 
             } else if (in->get_name() == "Instruction_cjump"){
@@ -62,23 +226,45 @@ namespace L2{
                 int64_t succ_position = find_label(label_name, &instructs);
                 nodes[i]->succs.insert(succ_position);
                 nodes[succ_position]->preds.insert(i);
-                std::cout << "succ position: " << succ_position << "\n";
-                std::cout << "pred position: " << i << "\n";
+                // std::cout << "succ position: " << succ_position << "\n";
+                // std::cout << "pred position: " << i << "\n";
             } else {
                 nodes[i]->succs.insert(i+1);
                 nodes[i+1]->preds.insert(i);
-                std::cout << "succ position: " << i+1 << "\n";
-                std::cout << "pred position: " << i << "\n";
+                // std::cout << "succ position: " << i+1 << "\n";
+                // std::cout << "pred position: " << i << "\n";
             }
         }
+
+        //gen kill sets
+        for (Node* n : nodes){
+            gen_kill(n);
+
+        }
+
+
+
+        // for (int i = 0; i < instructs.size(); i++){
+        //     std::cout << "pred for row " << i << "\n";
+        //     for (int64_t num: nodes[i]->preds){
+        //         std::cout << num << ' ';
+        //     }
+        //     std::cout << "\n\n";
+        //     std::cout << "succs for row " << i << "\n";
+        //     for (int64_t num: nodes[i]->succs){
+        //         std::cout << num << " " ;
+        //     }
+        //     std::cout << "\n\n";
+        // }
+
         for (int i = 0; i < instructs.size(); i++){
-            std::cout << "pred for row " << i << "\n";
-            for (int64_t num: nodes[i]->preds){
+            std::cout << "gen for row " << i << "\n";
+            for (std::string num: nodes[i]->gen){
                 std::cout << num << ' ';
             }
             std::cout << "\n\n";
-            std::cout << "succs for row " << i << "\n";
-            for (int64_t num: nodes[i]->succs){
+            std::cout << "kill for row " << i << "\n";
+            for (std::string num: nodes[i]->kill){
                 std::cout << num << " " ;
             }
             std::cout << "\n\n";
