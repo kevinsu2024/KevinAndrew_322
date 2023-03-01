41ca641a423739d9bb0446c4bec3040f04cdbb1d
Andrew W Lee <awl3193@moore.wot.eecs.northwestern.edu>
2023-03-01 01:36:20 -0600

    finished basic block

--- 322_framework/LA/src/basic_block_generator.cpp
+++ 322_framework/LA/src/basic_block_generator.cpp
@@ -2,7 +2,7 @@
 
 namespace LA{
     std::vector<Instruction*>
-    get_basic_blocks(std::vector<Instruction*> old, std::string longest_name, std::string return_type){
+    get_basic_blocks(std::vector<Instruction*> old, std::string longest_label, std::string return_type){
         std::vector<Instruction*> new_instrs;
         std::set<std::string> terminator_instrs = {"Instruction_return", "Instruction_return_t", "Instruction_branch", "Instruction_branch_t"};
         int64_t ctr = 0;
@@ -11,7 +11,7 @@ namespace LA{
             Instruction* in = old[ctr];
             if(startBB){
                 if(in->get_name() != "Instruction_label"){
-                    Instruction* new_label_instr = new Instruction_label(new InstructionLabel(longest_name + "_temp_label_name_" + std::to_string(ctr)));
+                    Instruction* new_label_instr = new Instruction_label(new InstructionLabel(longest_label + "_temp_label_name_" + std::to_string(ctr)));
                     new_instrs.push_back(new_label_instr);
                 }
                 startBB = false;
@@ -30,6 +30,30 @@ namespace LA{
             if(return_type == "void") in = new Instruction_return();
             else in = new Instruction_return_t(new InstructionNumber(0));
             new_instrs.push_back(in);
+        }
+
+        return new_instrs;
+    }
+
+    std::vector<Instruction*>
+    move_declarations(std::vector<Instruction*> old, std::string longest_label){
+        std::vector<Instruction*> new_instrs;
+        Instruction_label* first_in = (Instruction_label*) old[0];
+        Item* first_label = first_in->get_label();
+        new_instrs.push_back(new Instruction_label(new InstructionLabel(longest_label + "_entry")));
+        for(Instruction* in : old){
+            if(in->get_name() == "Instruction_declaration"){
+                new_instrs.push_back(in);
+                Instruction_declaration* instr = (Instruction_declaration*) in;
+                Item* var = instr->get_var();
+                if(instr->get_var_type()->to_string() == "code"){
+                    new_instrs.push_back(new Instruction_assignment(var, new InstructionNumber("0")));
+                }
+            }
+        }
+        new_instrs.push_back(new Instruction_branch(first_label));
+        for(Instruction* in : old){
+            if(in->get_name() != "Instruction_declaration") new_instrs.push_back(in);
         }
 
         return new_instrs;
